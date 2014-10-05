188a5ef2b730b3dbd05f8d69da81c9e7c93b2d82
hoamon+Acer@gmail.com <hoamon+Acer@gmail.com>
2014-10-05 15:17:54 +0800

    subscribe view_function needs staff_member_required permission; protocal and browse_host can get from request

--- sns/views.py
+++ sns/views.py
@@ -10,10 +10,12 @@ import logging, json
 logger = logging.getLogger(__name__)
 
 
+from django.contrib.admin.views.decorators import staff_member_required
 from django.http import HttpResponseBadRequest, HttpResponse
 from django.views.decorators.csrf import csrf_exempt
 
 from models import Subscription, Notification
+
 
 @csrf_exempt
 def sns_endpoint(request):
@@ -27,6 +29,11 @@ def sns_endpoint(request):
     return HttpResponse(json.dumps({'status': obj.status, 'message':message}), mimetype="application/json")
 
 
+@staff_member_required
 def subscribe(request, topic):
-    subscription = Subscription.subscribe(topic)
+    browse_host = request.META.get('HTTP_ORIGIN', '%s://%s' %
+                                    (request.META.get('wsgi.url_scheme', 'https'),
+                                        request.META['HTTP_HOST']))
+    protocal, dev_null = browse_host.split('://')
+    subscription = Subscription.subscribe(topic, protocal, browse_host)
     return HttpResponse(subscription.message, mimetype="application/json")
