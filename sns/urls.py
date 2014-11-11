13fe2af539ddf363cfdc14e9bad2d52bddb381e9
hoamon+Acer@ho600.com <hoamon+Acer@ho600.com>
2014-11-11 21:05:37 +0800

    upgrade for django-1.6

--- sns/urls.py
+++ sns/urls.py
@@ -1,4 +1,4 @@
-from django.conf.urls.defaults import patterns, url
+from django.conf.urls import patterns, url
 
 urlpatterns = patterns('sns.views',
     url(r'^endpoint/$', 'sns_endpoint', name='sns_endpoint'),
