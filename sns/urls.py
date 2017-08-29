ac0a6c41e100db96343f9d1b473cbad4ec783516
Max Mautner <max.mautner@gmail.com>
2017-08-29 11:22:25 -0700

    RemovedInDjango20Warning: specify app_name in urls.py
    RemovedInDjango20Warning: Specifying a namespace in
    django.conf.urls.include() without providing an app_name is deprecated.
    Set the app_name attribute in the included module, or pass a 2-tuple
    containing the list of patterns and app_name instead.

--- sns/urls.py
+++ sns/urls.py
@@ -1,6 +1,9 @@
 from django.conf.urls import url
 from sns.views import sns_endpoint, subscribe
 
+
+app_name = 'sns'
+
 urlpatterns = [
     url(r'^$', sns_endpoint, name='sns_endpoint'),
     url(r'^subscribe/(?P<topic>[^/]+)/$', subscribe, name='subscribe'),
